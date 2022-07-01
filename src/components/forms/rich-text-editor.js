2b721b10eb501918441498829f24e587095a3b0c
MrEclipse13 <52216179+MrEclipse13@users.noreply.github.com>
2022-06-30 23:36:56 -0500

    89%

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -3,16 +3,20 @@ import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
+import { resolve } from "path";
+import { rejects } from "assert";
 
 export default class RichTextEditor extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      editorState: EditorState.createEmpty(),
+      editorState: EditorState.createEmpty()
     };
 
     this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.getBase64 = this.getBase64.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
   }
 
   onEditorStateChange(editorState) {
@@ -24,6 +28,19 @@ export default class RichTextEditor extends Component {
     );
   }
 
+  getBase64(file, callback) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => callback(reader.result);
+    reader.onerror = error => {};
+  }
+
+  uploadFile(file) {
+    return new Promise((resolve, reject) => {
+      this.getBase64(file, data => resolve({ data: { link: data } }));
+    });
+  }
+
   render() {
     return (
       <div>
@@ -32,9 +49,28 @@ export default class RichTextEditor extends Component {
           wrapperClassName="demo-wrapper"
           editorClassname="demo-editor"
           onEditorStateChange={this.onEditorStateChange}
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: this.uploadFile,
+              alt: { present: true, mandatory: false },
+              previewImage: true,
+              inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
+            }
+          }}
         />
       </div>
     );
   }
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
 
+    Terms
+    Privacy
+    Security
